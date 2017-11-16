fe90ae628d556d40137da51125101a7290730164
DANIEL HENRIQUE PEREIRA <danihpe@hotmail.com>
2017-11-16 10:57:19 -0200

    Implementação da inserção na Hash

--- Pesquisa/src/Hash.java
+++ Pesquisa/src/Hash.java
@@ -1,9 +1,6 @@
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
+import java.util.HashMap;
 
 /**
  *
@@ -27,8 +24,7 @@ public class Hash {
      */
     
     public int h;
- 
-    
+   
     //Construtor recebe vetores
     public Hash(int[] vetor1, int[] vetor2, int[] vetor3){
         this.vetor1 = vetor1;
@@ -36,43 +32,53 @@ public class Hash {
         this.vetor3 = vetor3;
     }
     
+    //Método de inserção dos dados do Vetor 1 na HASH
+    //Utilizando a Função de Hash (ou de Transformação), é feita a inserção
+    //Detecção de colisões também implementada, caso a posição esteja ocupada, insere-se elemento no próximo campo vazio
     public void insercaoHash(){
         
-        int prox = 0; //Ponteiro
-        int auxiliar = 0;
+        int auxiliar = 0; 
         int j = 0;
           
         for(int i = 0; i < tabelaHash1.length; i++){
-            tabelaHash1[i] = -1;
+            tabelaHash1[i] = -1; //Todos os valores da Hash recebem -1 pra indicar campo vazio 
         }
 
-        for(int i = 0; i < vetor1.length; i++){
+        for(int i = 0; i < vetor1.length; i++){ 
             
-            h = vetor1[i] % tabelaHash1.length; 
-            auxiliar = h;
-            if(tabelaHash1[auxiliar] == -1){
-                tabelaHash1[auxiliar] = vetor1[i];
-            } else {
-                for(j = auxiliar; j < tabelaHash1.length; j++){
-                    if(tabelaHash1[j] == -1){
-                        tabelaHash1[j] = vetor1[i];
-                        break;
+            h = vetor1[i] % tabelaHash1.length; //Função de transformação
+            auxiliar = h; //Auxiliar funciona como uma espécie de ponteiro
+            if(tabelaHash1[auxiliar] == -1){ //Se Hash na posição auxiliar for -1 (Vazia)
+                tabelaHash1[auxiliar] = vetor1[i]; //Hash na posição auxiliar recebe a chave
+            } else { //Se posição não for -1 (Não vazia)
+                for(j = auxiliar; j < tabelaHash1.length; j++){ //Percorre a hash até o final em busca do próximo campo vazio
+                    if(tabelaHash1[j] == -1){ //Se encontrar a posição vazia
+                        tabelaHash1[j] = vetor1[i]; //Hash nessa posição (j) recebe a chave
+                        break; //Imediatamente após isso, break na condição 
                     }
-                    if(j >= tabelaHash1.length - 1){
-                        j = 0;
-                        auxiliar = j;
+                    if(j >= tabelaHash1.length - 1){ //Se j for maior ou igual ao tamanho da Hash -1
+                        j = 0; //Reinicia-se j para buscar campos vazios anteriores
+                        auxiliar = j; //Auxiliar recebe o valor reiniciado de j
                     }
                 }
                        
             }
         }
         
+        //Impressão da Hash criada
         System.out.print("Hash: ");
         for(int i = 0; i < tabelaHash1.length; i++){
             System.out.print(tabelaHash1[i] + " ");
         }
         
         System.out.println("\n");
+        
+    }
+    
+    //Pesquisa de valores na Hash
+    public void pesquisaHash(){
+        
+        
         
     }
     
